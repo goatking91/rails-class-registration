e709836a5890e770d5c6b6a5aa8d6a64ef91da4d
Selim Yeom <selim.yeom@lfin.kr>
2022-09-23 17:00:25 +0900

    🔧 application_record.rb 설정

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,25 @@
+# frozen_string_literal: true
+
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.generate_unique_secure_token(length = nil)
+    # uuid v4
+    SecureRandom.uuid
+  end
+
+  scope :paginate, -> (params) {
+    if params.respond_to?(:permit)
+      permitted = params.permit(:page, :size)
+    else
+      permitted = params
+    end
+    page = permitted.fetch(:page, 1).to_i
+    size = permitted.fetch(:size, 100).to_i
+
+    page = [1, page].max
+    size = [100, size].min
+
+    page(page).per(size)
+  }
 end
