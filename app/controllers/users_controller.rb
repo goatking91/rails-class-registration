53e37a0f53f85e79875ca87607cdc787cee1b194
Selim Yeom <selim.yeom@lfin.kr>
2022-09-26 15:49:50 +0900

    ✨ 유저 수강권 등록 컨트롤러에 서비스 추가

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: [:lessons]
+
   def create
     result = Users::CreateService.run!(create_params)
+    render api: result, status: :created
+  end
+
+  def lessons
+    result = Lessons::CreateService.run!(lessons_params)
     render api: result, status: :created
   end
 
   private
     def create_params
       api_params.permit(:name, :cellphone)
+    end
+
+    def lessons_params
+      api_params.permit(:minute, :content).merge(user: @user)
+    end
+
+    def set_user
+      uid = api_params.permit(:uid).fetch(:uid, nil)
+      @user = User.find_by(uid:)
     end
 end
