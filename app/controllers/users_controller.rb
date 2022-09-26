932d9550ea1f609fad5f3ca1855a2a9b9cf9833b
Selim Yeom <selim.yeom@lfin.kr>
2022-09-26 18:35:39 +0900

    🎨 수강 신청 서비스 컨트롤러에 추가

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: [:create_lessons, :index_lessons, :index_tutors]
+  before_action :set_user, only: [:create_lessons, :index_lessons, :index_tutors, :sign_up]
 
   def create
     result = Users::CreateService.run!(create_params)
@@ -23,6 +23,10 @@ class UsersController < ApplicationController
     render api: result, status: :ok
   end
 
+  def sign_up
+    Lessons::SignUpService.run!(sign_up_params)
+  end
+
   private
     def create_params
       api_params.permit(:name, :cellphone)
@@ -38,6 +42,12 @@ class UsersController < ApplicationController
 
     def index_tutors_params
       api_params.permit(:start_time, :minute).merge(user: @user)
+    end
+
+    def sign_up_params
+      tutor = api_params.permit(tutor: [:id]).dig(:tutor).fetch(:id, nil)
+      lesson = api_params.permit(lesson: [:id]).dig(:lesson).fetch(:id, nil)
+      api_params.permit(:start_time).merge(user: @user, tutor:, lesson:)
     end
 
     def set_user
